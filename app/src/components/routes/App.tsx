9d9d07c772014a519673634ff825ed5c4bbae139
kei <dr.kei21@gmail.com>
2024-08-25 15:46:07 +0900

    feat: ルーティングの追加

--- app/src/components/routes/App.tsx
+++ app/src/components/routes/App.tsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../../pages/Home";
-import Title from "../../pages/Title";
+import Top from "../../pages/Top/Top";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Title />} />
+      <Route path="/" element={<Top />} />
       <Route path="/home" element={<Home />} />
     </Routes>
   );
