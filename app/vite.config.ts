d27c8ca7e25088f9208b51a69452fc0078526e2b
kei <dr.kei21@gmail.com>
2025-03-09 04:00:15 +0900

    feat: 開発環境の設定を更新

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,9 +1,13 @@
-// vite.config.js
+// vite.config.ts
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/",
   server: {
     host: "0.0.0.0", // すべてのネットワークインターフェースでリッスン
     port: 5173, // ポート番号
+  },
+  preview: {
+    port: 5173,
   },
 });
