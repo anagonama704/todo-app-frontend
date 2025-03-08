fb8780bbc35ea8aa02367444bb5809a29ee3f582
kei <dr.kei21@gmail.com>
2025-03-09 04:01:01 +0900

    feat: エントリーポイントとテストを追加

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,21 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./assets/index.css";
 import App from "./components/routes/App";
 import reportWebVitals from "./reportWebVitals";
 import "@mantine/core/styles.css";
 import Provider from "./components/providers/Provider";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <Provider>
-      <App />
-    </Provider>
-  </React.StrictMode>
-);
+async function initApp() {
+  if (process.env.NODE_ENV === "development") {
+    const { worker } = await import("./mocks/browser");
+    await worker.start({
+      onUnhandledRequest: "bypass",
+    });
+  }
+
+  const root = ReactDOM.createRoot(
+    document.getElementById("root") as HTMLElement
+  );
+  root.render(
+    <React.StrictMode>
+      <Provider>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  );
+}
+
+initApp();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
