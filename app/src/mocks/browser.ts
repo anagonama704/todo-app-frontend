e524045333a447fd6c1a2398e799dd8972182937
kei <dr.kei21@gmail.com>
2025-03-12 19:53:29 +0900

    fix: 未使用の関数を削除

--- app/src/mocks/browser.ts
+++ app/src/mocks/browser.ts
@@ -1,12 +1,6 @@
 import { setupWorker } from "msw/browser";
 import { handlers } from "./handlers";
 
-// 環境に応じたService Worker URLを設定
-const getWorkerUrl = () => {
-  // 常に現在のオリジンからの相対パスを使用
-  return "/mockServiceWorker.js";
-};
-
 // workerの設定
 export const worker = setupWorker(...handlers);
 
